4c047f70383f95e5fbce3d6cc6b76cfd397ccd82
vectronic <hello.vectronic@gmail.com>
2024-06-13 17:17:53 +0100

    pending-0021-PATCH-1-1-avformat-dashdec-prevent-endless-loop-on-buffer-refill.patch

--- libavformat/dashdec.c
+++ libavformat/dashdec.c
@@ -1860,6 +1860,8 @@ restart:
     /* check the v->cur_seg, if it is null, get current and double check if the new v->cur_seg*/
     if (!v->cur_seg) {
         v->cur_seg = get_current_fragment(v);
+        v->cur_seg_size = v->cur_seg->size;
+        v->cur_seg_offset = 0;
     }
     if (!v->cur_seg) {
         ret = AVERROR_EOF;
@@ -1868,6 +1870,12 @@ restart:
     ret = read_from_url(v, v->cur_seg, buf, buf_size);
     if (ret > 0)
         goto end;
+
+    if (ret == 0) {
+        v->cur_seq_no++;
+        free_fragment(&v->cur_seg);
+        goto restart;
+    }
 
     if (c->is_live || v->cur_seq_no < v->last_seq_no) {
         if (!v->is_restart_needed)
