1b2a605345f5251d83294cb11973684aeda000e4
vectronic <hello.vectronic@gmail.com>
2026-07-20 19:10:08 +1000

    pending-0020-PATCH-1-1-ffprobe-prevent-endless-subtitle-flush.patch

--- fftools/ffprobe.c
+++ fftools/ffprobe.c
@@ -1699,7 +1699,10 @@ static int read_interval_packets(AVTextFormatContext *tfc, InputFile *ifile,
     for (i = 0; i < ifile->nb_streams; i++) {
         pkt->stream_index = i;
         if (do_read_frames) {
-            while (process_frame(tfc, ifile, frame, pkt, &(int){1}) > 0);
+            int new_packet = 1;
+            while (process_frame(tfc, ifile, frame, pkt, &new_packet) > 0) {
+                new_packet = 0;
+            }
             if (ifile->streams[i].dec_ctx)
                 avcodec_flush_buffers(ifile->streams[i].dec_ctx);
         }
